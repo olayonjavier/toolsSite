15a3b756832983caf923731ff88e5c4335e6821d
Javier Olayon <olayon.javier@gmail.com>
2020-07-08 08:04:20 -0400

    business timers page skeleton

---
+++ gta_tools/business_timers/bussiness.js
@@ -0,0 +1,8 @@
+
+function show()
+{
+  if (document.getElementById(gunrunning).checked) 
+  {
+      document.getElementById('gunStaffUpgrade').style.display = "block";
+      document.getElementById('gunEquipUpgrade').style.display = "block";
+}
